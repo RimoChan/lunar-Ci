2ddde7a7fd043c48838133913e232ec666e0c1e1
RimoChan <RimoChan@stu.xmu.edu.cn>
2021-04-23 21:55:04 +0800

    多写点然后更新版本号

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ import setuptools
 
 setuptools.setup(
     name='lunar_test',
-    version='0.0.1',
+    version='0.0.2',
     author='RimoChan',
     author_email='the@librian.net',
     description='lunar_test',
