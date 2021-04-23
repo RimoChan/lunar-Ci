6262a8c12fbee3fc06d02ffb8f0ad90edf2d40cd
RimoChan <RimoChan@stu.xmu.edu.cn>
2021-04-23 21:35:05 +0800

    加入代码

---
+++ lunar_test/__init__.py
@@ -0,0 +1,15 @@
+import unittest
+import datetime
+
+from lunar_python import Lunar
+
+
+def 咦():
+    today = datetime.datetime.today()
+    lunar = Lunar.fromYmd(today.year, today.month, today.day)
+    return '诸事不宜' not in lunar.getTimeJi()
+
+
+class _(unittest.TestCase):
+    def test(self):
+        assert 咦(), '今日诸事不宜！'
---
+++ setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+
+setuptools.setup(
+    name='lunar_test',
+    version='0.0.1',
+    author='RimoChan',
+    author_email='the@librian.net',
+    description='lunar_test',
+    long_description=open('readme.md', encoding='utf8').read(),
+    long_description_content_type='text/markdown',
+    url='https://github.com/RimoChan/lunar-Ci',
+    packages=['lunar_test'],
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'Operating System :: OS Independent',
+    ],
+    install_requires=[
+        'lunar-python>=1.0.29',
+    ],
+    python_requires='>=3.6',
+)
